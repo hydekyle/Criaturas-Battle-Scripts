cec4cf33a4c33d97efd2f1063653c8992ef0b17c
Hyde_Wick <hydekyle@hotmail.com>
2019-06-05 02:45:31 +0100

    no message

--- 0.HYDEREWORK/EquipTable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EquipD List", menuName = "Scriptables/EquipD List", order = 1)]
-public class EquipD_List : ScriptableObject
-{
-    public List<EquipD> helmets, armors, backs, weapons_1H, weapons_2H, shields;
-}
